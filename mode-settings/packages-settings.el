f0126ea5112d01479d86148d25fec5902eaf84be
John Sigman <john.sigman@gmail.com>
2023-04-20 15:55:13 -0400

    vterm stuff and helpful and magit

--- mode-settings/packages-settings.el
+++ mode-settings/packages-settings.el
@@ -23,9 +23,27 @@
 (use-package general)
 (use-package org :demand t)
 
-(use-package literate-elisp)
+(use-package
+ literate-elisp
+ :demand t
+ :config
+ ;; ;; To make `elisp-refs' work with `literate-elisp', we need to add an advice to `elisp-refs--read-all-buffer-forms'.
+ (eval-after-load "elisp-refs"
+   '(advice-add
+     'elisp-refs--read-all-buffer-forms
+     :around #'literate-elisp-refs--read-all-buffer-forms))
+
+ ;; To make `elisp-refs' work with `literate-elisp', we need to add an advice to `elisp-refs--loaded-paths'.
+ (eval-after-load "elisp-refs"
+   '(advice-add
+     'elisp-refs--loaded-paths
+     :filter-return #'literate-elisp-refs--loaded-paths))
+ ;; To make `helpful' work with `literate-elisp', we need to add an advice to `helpful--find-by-macroexpanding'.
+ (eval-after-load 'helpful
+   '(advice-add
+     'helpful--find-by-macroexpanding
+     :around #'literate-elisp-helpful--find-by-macroexpanding)))
+
 (require 'org-element) ; TODO: until org-element--cache-active-p autoload fixed in literate-elisp upstream
 
 (provide 'packages-settings)
-
-
