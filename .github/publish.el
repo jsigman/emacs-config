363bf171f67d064f0d949b24a7ecfa74437513c1
John Sigman <john.sigman@gmail.com>
2023-05-18 05:15:58 -0400

    script

---
+++ .github/publish.el
@@ -0,0 +1,12 @@
+(require 'org)
+(require 'ox-md)
+
+; Set current buffer to the publishable org buffer
+(find-file "web_version.org")
+(let ((md-buffer (find-file-noselect "../published_version.md")))
+(org-export-to-buffer 'md md-buffer
+    nil nil nil nil nil (lambda () (text-mode)))
+; Save the buffer
+(set-buffer md-buffer)
+;; write the buffer md-buffer to file
+(write-file "published_version.md"))
