e29bbad07873bce79029c682a43fe75d572e88dd
John Sigman <john.sigman@gmail.com>
2023-05-18 20:59:15 -0400

    test

--- .github/publish.el
+++ .github/publish.el
@@ -1,10 +1,16 @@
 (require 'org)
-(require 'ox-md)
+(require 'package)
+(setq use-package-verbose t
+          use-package-expand-minimally t)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(require 'use-package)
+(use-package ox-jekyll-md :ensure t)
 
 ; Set current buffer to the publishable org buffer
 (find-file "web_version.org")
 (let ((md-buffer (find-file-noselect "published_version.md")))
-(org-export-to-buffer 'md md-buffer
+(org-export-to-buffer 'jekyll md-buffer
     nil nil nil nil nil (lambda () (text-mode)))
 ; Save the buffer
 (set-buffer md-buffer)
