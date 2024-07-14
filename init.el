35d8c779667df8557ba89ea0bdbda49c02468e79
John Sigman <john.sigman@gmail.com>
2024-07-14 11:34:09 -0400

    new

--- init.el
+++ init.el
@@ -3,13 +3,15 @@
 ;; Packages setup to prepare literate-elisp
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+       (expand-file-name "straight/repos/straight.el/bootstrap.el"
+                         user-emacs-directory))
       (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
          "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
+         'silent
+         'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
@@ -23,30 +25,30 @@
 (use-package org)
 
 (use-package
-  literate-elisp
-  :demand t
-  :config
-  ;; ;; To make `elisp-refs' work with `literate-elisp', we need to add an advice to `elisp-refs--read-all-buffer-forms'.
-  (eval-after-load "elisp-refs"
-    '(advice-add
-      'elisp-refs--read-all-buffer-forms
-      :around #'literate-elisp-refs--read-all-buffer-forms))
-  ;; To make `elisp-refs' work with `literate-elisp', we need to add an advice to `elisp-refs--loaded-paths'.
-  (eval-after-load "elisp-refs"
-    '(advice-add
-      'elisp-refs--loaded-paths
-      :filter-return #'literate-elisp-refs--loaded-paths))
-  ;; To make `helpful' work with `literate-elisp', we need to add an advice to `helpful--find-by-macroexpanding'.
-  (eval-after-load 'helpful
-    '(advice-add
-      'helpful--find-by-macroexpanding
-      :around #'literate-elisp-helpful--find-by-macroexpanding)))
+ literate-elisp
+ :demand t
+ :config
+ ;; ;; To make `elisp-refs' work with `literate-elisp', we need to add an advice to `elisp-refs--read-all-buffer-forms'.
+ (eval-after-load "elisp-refs"
+   '(advice-add
+     'elisp-refs--read-all-buffer-forms
+     :around #'literate-elisp-refs--read-all-buffer-forms))
+ ;; To make `elisp-refs' work with `literate-elisp', we need to add an advice to `elisp-refs--loaded-paths'.
+ (eval-after-load "elisp-refs"
+   '(advice-add
+     'elisp-refs--loaded-paths
+     :filter-return #'literate-elisp-refs--loaded-paths))
+ ;; To make `helpful' work with `literate-elisp', we need to add an advice to `helpful--find-by-macroexpanding'.
+ (eval-after-load 'helpful
+   '(advice-add
+     'helpful--find-by-macroexpanding
+     :around #'literate-elisp-helpful--find-by-macroexpanding)))
 
 ; Start literate load
-(literate-elisp-load (expand-file-name "init.org" user-emacs-directory))
+(literate-elisp-load
+ (expand-file-name "init.org" user-emacs-directory))
 
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (if (not (file-exists-p custom-file))
     (f-touch custom-file))
 (load custom-file)
-
